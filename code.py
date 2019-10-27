e18ca38bb7a1dfba3732c3fd7adc41e9ab62683a
Mikhail Kozhevnikov <misha0613@gmail.com>
2019-10-27 15:39:27 -0400

    added a py

---
+++ code.py
@@ -0,0 +1,19 @@
+import sys
+import math
+import random
+from random import choices
+import fileinput
+from collections import defaultdict
+import glob, os 
+
+def main():
+    word = "I'm blue da ba dee da ba daa "
+    #for i in range(0, 50):
+        #word += (" daa ", " ba ", " dee ")
+
+    print(word)
+
+
+#Charles Daniels told me this is good practice during an ACM meeting
+if __name__ == "__main__":
+    main()
