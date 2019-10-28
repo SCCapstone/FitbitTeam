01662eba651bf0c0a064d72fb11dec435314d51e
Christopher Walls <clwalls@email.sc.edu>
2019-10-27 23:54:06 -0400

    added comparsion
    comparison to check value of an int to determine what string to print

--- code.py
+++ code.py
@@ -8,13 +8,18 @@ import glob, os
 
 def main():
     word = "I'm blue da ba dee da ba daa "
+    third_law = "For every action there is an equal and opposite reaction."
+    num_law = 3
+
     #for i in range(0, 50):
         #word += (" daa ", " ba ", " dee ")
-
     print(word)
     a = 2
     b = 3
     print(a+b)
+    if num_law == 3:
+      print(third_law)
+    
 
 #Charles Daniels told me this is good practice during an ACM meeting
 if __name__ == "__main__":
