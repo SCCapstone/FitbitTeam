9286ebfba4b58fc4fe3fef5e4fa0b2a39dc431f2
Tyler Nunley <Tnunley@email.sc.edu>
2019-11-24 12:40:28 -0500

    Work on signup

--- FitbitTeam/src/app/app.module.ts
+++ FitbitTeam/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { RecoveryComponent } from './recovery/recovery.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- FitbitTeam/src/app/signup/signup.component.ts
+++ FitbitTeam/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  user = {
+    email: '',
+    password: '',
+    first_name: '',
+    last_name:'',
+    type: ''
+  };
   constructor() { }
 
   ngOnInit() {
   }
+  signup() {
+    const email = this.user.email;
+    const password = this.user.password;
+    const first_name = this.user.first_name;
+    const last_name = this.user.last_name;
+    const type= this.user.type;
+    const self= this;
+
+    console.log("This runs");
+    console.log(this.user);
+
+    
+  }
+
 
 }
