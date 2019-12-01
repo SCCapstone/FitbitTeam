757d6bee31d32921a825ef074dfd90bb86c3eaa8
Tyler Nunley <Tnunley@email.sc.edu>
2019-12-01 18:11:51 -0500

    more changes

--- FitbitTeam/server.js
+++ FitbitTeam/server.js
@@ -3,11 +3,8 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-
 app.listen(process.env.PORT || 8080);
-
-//PathLocationStrategy
-app.get('/*', function(req, res){
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
