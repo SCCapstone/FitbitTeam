86ea260575a1d67cd630b2089c50b69fd341b351
Tyler Nunley <Tnunley@email.sc.edu>
2019-12-01 18:04:16 -0500

    heroku

--- FitbitTeam/server.js
+++ FitbitTeam/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(_dirname+ '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 app.get('/*', function(req, res){
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 console.log('console listen');
