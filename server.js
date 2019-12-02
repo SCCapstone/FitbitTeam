eb9e2a156c1215c351965837a5d23d67ca4d81b2
Tyler Nunley <Tnunley@email.sc.edu>
2019-12-01 20:12:32 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname + '/dist'));
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/fitbitteam'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/fitbitteam/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-console.log('console listen');
