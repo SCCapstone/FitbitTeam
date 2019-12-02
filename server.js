58280d3e16a668a66c2ad526fba19b4103092a6b
Tyler Nunley <Tnunley@email.sc.edu>
2019-12-01 20:03:13 -0500

    stuff

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 console.log('console listen');
