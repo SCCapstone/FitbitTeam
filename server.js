97029cd630ee3c4f1598b5a5ad168712e349d2ab
mzaleski98 <mzaleski@email.sc.edu>
2019-12-03 04:07:48 -0500

    Heroku testing

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/fitbitteam'));
+app.use(express.static(__dirname + '/dist/FitbitTeam'));
+
+// Set port of app
+// process.env lets port be set by heroku
+var port = process.env.PORT || 8080;
+app.listen(process.env.PORT || 8080);
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/fitbitteam/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/FitbitTeam/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+console.log('Console Listening');
