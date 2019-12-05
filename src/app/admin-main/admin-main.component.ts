287822bf6d9fbf5d04949eaa88d71efc429386da
Mikhail Kozhevnikov <misha0613@gmail.com>
2019-12-04 23:32:01 -0500

    implemented boilerplate timeline

--- src/app/admin-main/admin-main.component.ts
+++ src/app/admin-main/admin-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,Routes, RouterModule , ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-main',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router,private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
+  toTimeline(){
+    this.router.navigate(["../timeline"])
 
+  }
 }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -10,6 +10,25 @@ export class TimelineComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var x = document.getElementById("myChartDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-
+  }
+  toggle() {
+    var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("myChartDIV");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+  }
 }
