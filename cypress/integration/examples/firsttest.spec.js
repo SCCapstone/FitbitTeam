3a2550aaf2d73fce8b2fb61189b397757fc20b60
Mikhail Kozhevnikov <misha0613@gmail.com>
2020-02-02 18:44:12 -0500

    finished first testing file

--- cypress/integration/examples/firsttest.spec.js
+++ cypress/integration/examples/firsttest.spec.js
@@ -21,6 +21,54 @@ describe('first test', () => {
   })
   it('client settings', () => {
     cy.get('a[routerLink="../settings"]').click()
-    
   })
+  it('client settings alexa', () => {
+    //cy.get('a[routerLink=""]').click()
+  })
+  it('client settings fitbit', () => {
+    //cy.get('a[routerLink=""]').click()
+  })
+  it('client settings name change', () => {
+    cy.get('button[class="btn btn-md btn-primary"]').click()
+    cy.get('input[placeholder="First Name"]').type('test')
+    cy.get('input[placeholder="Last Name"]').type('test')
+    cy.contains('Save').click()
+    cy.get('a[routerLink="../cmain"]').click()
+  })
+  
+  it('client timeline', () => {
+    cy.get('img').click()
+    cy.contains('Week').click()
+    cy.contains('Month').click()
+    cy.contains('Year').click()
+    cy.contains('Table').click()
+    cy.get('a[routerLink="../cmain"]').click()
+  })
+  it('logout', () => {
+    cy.get('a[routerLink="../settings"]').click()
+    cy.contains('Logout').click()
+
+  })
+  it('admin login', () => {
+    cy.get('input[id=InputUser').type('{selectall}{backspace}')
+    cy.get('input[id=InputUser').type('admin@email.com')
+    cy.get('input[id=InputPassword').type('123456')
+    cy.get('a[class="btn btn-lg btn-success"]').click()
+
+  })
+  it('adding a patient', () => {
+    cy.contains('Add').click()
+    cy.contains('Back').click()
+    cy.contains('Add').click()
+    cy.get('input[placeholder="Name"]').type('test')
+    cy.get('input[placeholder="Reference Number"]').type('test123')
+    cy.contains('Add').click()
+
+  })
+  it('logout', () => {
+    cy.get('a[routerLink="../asettings"]').click()
+    cy.contains('Logout').click()
+
+  })
+
 })
