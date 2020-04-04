16378904795d58ab7fe4503912e9c0ccd42dc783
Tyler Nunley <Tnunley@email.sc.edu>
2020-04-04 18:42:33 -0400

    fixed signup bug

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -12,6 +12,7 @@ import * as firebase from 'firebase';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  
   user = {
     email: '',
     password: '',
@@ -21,7 +22,13 @@ export class SignupComponent implements OnInit {
   };
   constructor() { }
   ngOnInit() {
+    firebase.auth().signOut().then(function() {
+    }).catch(function(error) {
+      // An error happened.
+    });
+    console.log(firebase.auth().currentUser.uid)
   }
+
   signup() {
     var email = this.user.email;
     var password = this.user.password;
@@ -30,6 +37,7 @@ export class SignupComponent implements OnInit {
     const type= this.user.type;
     const status = 'Healthy'
     const self= this;
+    
 
     console.log(this.user);
     //Database stuff
@@ -40,11 +48,14 @@ export class SignupComponent implements OnInit {
       if (errorCode.length > 0) {
         console.log('Failed');
         alert(errorMessage)
+        this.failsafe=false;
       } else {
         console.log('Signed-UP');
       }
       })
       .then((user) => {
+     
+          console.log(firebase.auth().currentUser.uid)
           const userid = firebase.auth().currentUser.uid;
           const usertype = firebase.database().ref('usertypes/'+userid);
           usertype.set({
@@ -55,6 +66,8 @@ export class SignupComponent implements OnInit {
             'type': type
           });
           console.log(usertype)
+        
+        
       });
   }
 }
